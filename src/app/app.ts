cbbd4a6c1de570538c53129c5a71e9dab7e44517
DevvKuba <jrfitnessinbox@gmail.com>
2026-06-07 18:23:49 +0100

    Implement separate methodology component beneath landing page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { MethodologyComponent } from './components/methodology/methodology.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, HeroComponent],
+  imports: [NavbarComponent, HeroComponent, MethodologyComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ src/app/components/methodology/methodology.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-methodology',
+  standalone: true,
+  templateUrl: './methodology.component.html',
+  styleUrl: './methodology.component.scss',
+})
+export class MethodologyComponent {}
