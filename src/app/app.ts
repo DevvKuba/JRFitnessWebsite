33d26d466748c6a5a3f36a75f8811ecfb598d9d4
DevvKuba <jrfitnessinbox@gmail.com>
2026-06-07 19:15:16 +0100

    Finalize website footer page containing coaching location & social icons

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { HeroComponent } from './components/hero/hero.component';
 import { MethodologyComponent } from './components/methodology/methodology.component';
 import { OfferingsComponent } from './components/offerings/offerings.component';
 import { TransformationsComponent } from './components/transformations/transformations.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, HeroComponent, MethodologyComponent, OfferingsComponent, TransformationsComponent],
+  imports: [NavbarComponent, HeroComponent, MethodologyComponent, OfferingsComponent, TransformationsComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
