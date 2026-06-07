4c3f17b78fee38cfae19052b69634684d5a7124b
DevvKuba <jrfitnessinbox@gmail.com>
2026-06-07 18:30:23 +0100

    Create offerings component, referring to personal training services

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { MethodologyComponent } from './components/methodology/methodology.component';
+import { OfferingsComponent } from './components/offerings/offerings.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, HeroComponent, MethodologyComponent],
+  imports: [NavbarComponent, HeroComponent, MethodologyComponent, OfferingsComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ src/app/components/offerings/offerings.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offerings',
+  standalone: true,
+  templateUrl: './offerings.component.html',
+  styleUrl: './offerings.component.scss',
+})
+export class OfferingsComponent {}
